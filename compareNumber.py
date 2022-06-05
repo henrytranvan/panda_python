e88d4b988da6a947dfbc282d587b2dcfb11b8789
giahuytrannguyen <giahuy.trannguyen68@gmail.com>
2022-06-05 01:38:27 +0000

    panda

--- compareNumber.py
+++ compareNumber.py
@@ -1,12 +1,19 @@
-a = input('enter number a=?')
-b = input('enter number b=?')
 
-if a>b:
+def compareFuction(a,b):
+  print('a = ' + str(a))
+  print('a =' + str(b))
+  if a>b:
     print('a is greater than b ')
-elif a==b:
+  elif a==b:
     print('a is equal to b')
-else:
+  else:
     print('a is less than b')
+
+a = input('enter number a=')
+b = input('enter number b=')
+compareFuction(a, b)
+
+
 # command S saves
 
 
