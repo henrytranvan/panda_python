c25d8443421635d8fd9b40b657116b8dc38870a7
giahuytrannguyen <giahuy.trannguyen68@gmail.com>
2022-06-05 04:31:05 +0000

    panda

---
+++ Pandacalculator.py
@@ -0,0 +1,7 @@
+a = input('Please enter a number a = ')
+b = input('Please enter a number b = ')
+c = input('Please enter a number c = ')
+
+sum = int(a) + int(b) + int(c)
+print(a + " + "+ b +' + ' + c + " = " + str(sum))
+
