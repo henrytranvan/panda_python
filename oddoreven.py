d61368577b6d9763d4ca6c40e3d17aeb3f2f57d5
giahuytrannguyen <giahuy.trannguyen68@gmail.com>
2022-06-05 04:20:07 +0000

    panda

--- oddoreven.py
+++ oddoreven.py
@@ -1,5 +1,12 @@
+
+def oddOrEven(number):
+   if (number % 2) == 0:
+      print(str(number) + " is Even")
+   else:
+      print(str(number) + "  is Odd" )
+
+
+
+
 num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num) )
+oddOrEven(num)
