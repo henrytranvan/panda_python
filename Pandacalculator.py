6b82b98a5983f3b564e62a39b70813a149b8782d
giahuytrannguyen <giahuy.trannguyen68@gmail.com>
2022-06-05 05:11:05 +0000

    panda

--- Pandacalculator.py
+++ Pandacalculator.py
@@ -7,3 +7,15 @@ print(a + " + "+ b +' + ' + c + " = " + str(sum))
 
 sum1 = int(a) - int(b) + int(c)
 print(a + " - "+ b +' + ' + c + " = " + str(sum1))
+
+sum2 = int(a) * int(b)  + int(c)
+print(a + " * " + b + " + " + c + " = "+ str(sum2))
+
+sum3 = int(a) / int(b)  + int(c)
+print(a + " / " + b + " + " + c + " = "+ str(sum3))
+
+sum4 = int(a) - int(b)  - int(c)
+print(a + " - " + b + " - " + c + " = "+ str(sum4))
+sum2 = int(a) * int(b)  + int(c)
+print(a + " * " + b + " * " + c + " = "+ str(sum2))
+
