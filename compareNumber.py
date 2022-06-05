88842ba0391db9c2d36898d770387a420d4c4330
giahuytrannguyen <giahuy.trannguyen68@gmail.com>
2022-06-05 01:26:09 +0000

    panda

---
+++ compareNumber.py
@@ -0,0 +1,12 @@
+a = input('enter number a=?')
+b = input('enter number b=?')
+
+if a>b:
+    print('a is greater than b ')
+elif a==b:
+    print('a is equal to b')
+else:
+    print('a is less than b')
+# command S saves
+
+
