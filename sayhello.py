7e342c0e68ab7583f67031235eb36f138cc9a6e4
giahuytrannguyen <giahuy.trannguyen68@gmail.com>
2022-06-05 02:58:20 +0000

    panda

--- sayhello.py
+++ sayhello.py
@@ -5,6 +5,15 @@ hour = datetime.now().time().hour # time object
 print("sever time =", hour)
 print("type(now) =", type(hour))	
 
-if hour < 12:
-    print('Good morning')
-    
+
+def sayhello(hour):
+  if hour < 12:
+     print('Good morning')
+  elif hour <18:
+    print('Good afternoon')
+  else:
+    print('Good night!')
+
+sayhello(hour)
+sayhello(14)
+sayhello(20)
