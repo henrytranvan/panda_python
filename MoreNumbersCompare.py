4c80e8b5649e9c0dae76490db75f06c7892fe37f
giahuytrannguyen <giahuy.trannguyen68@gmail.com>
2022-06-08 09:44:33 +0000

    panda

--- MoreNumbersCompare.py
+++ MoreNumbersCompare.py
@@ -1,19 +1,16 @@
-
 def compareFuction(a,b):
   print('a = ' + str(a))
   print('a =' + str(b))
-  if a>b:
+  if a>b>c:
     print('a is greater than b ')
-  elif a==b:
-    print('A is equal to b')
+  elif a==b==c:
+    print('A is equal to b ')
   else:
-    print('b is less than a')
+    print('b is less than a' )
 print('USE REAL NUMBERS OR ELSE IT WILL NOT WORK')
 a = input('enter number a=')
 b = input('enter number b=')
+c = input('enter number c=')
 compareFuction(a, b)
-
-
-# press command + S to save file
 
 
