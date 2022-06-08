c4848114148fca1ab82d366f7809c4c2d29241b9
Henry Tran <henry.tranvan@gmail.com>
2022-06-08 10:32:22 +0000

    Panda

--- MoreNumbersCompare.py
+++ MoreNumbersCompare.py
@@ -1,16 +1,22 @@
-def compareFuction(a,b):
-  print('a = ' + str(a))
-  print('a =' + str(b))
-  if a>b>c:
-    print('a is greater than b ')
-  elif a==b==c:
-    print('A is equal to b ')
-  else:
-    print('b is less than a' )
+# This is function defination.
+def compareFuction(a,b,c):  
+  if a > b:
+    if a > c:
+       print('a is biggest number')
+    else: # a<c
+      print('c is biggest number')
+  elif b > c: # a < b
+    print('b is biggest number')
+  else: # a<b<c
+    print('c is biggest number')
+
+
+# This is the main program, it start run from here when you start
 print('USE REAL NUMBERS OR ELSE IT WILL NOT WORK')
 a = input('enter number a=')
 b = input('enter number b=')
 c = input('enter number c=')
-compareFuction(a, b)
+# Call the function with real variable values input from user, function will run.
+compareFuction(a, b, c)
 
 
