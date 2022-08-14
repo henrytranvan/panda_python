4c3f9f2087f1b7a400a14c3af8c7484e36de8c7e
giahuytrannguyen <giahuy.trannguyen68@gmail.com>
2022-08-14 03:16:21 +0000

    Panda

--- MoreNumbersCompare.py
+++ MoreNumbersCompare.py
@@ -10,7 +10,7 @@ def compareFuction(a,b,c):
   else: # a<b<c
     print('c is biggest number')
 
-
+# lies
 # This is the main program, it start run from here when you start
 print('USE REAL NUMBERS OR ELSE IT WILL NOT WORK')
 a = input('enter number a=')
--- lession2/Chatbot2.0.py
+++ lession2/Chatbot2.0.py
@@ -23,34 +23,34 @@ ChatBot_Name = "chat_bot1"
 print('commands are: Who are you?,Hi,Quit,what version is it?,I am sad,why,Your not real, But why?, But really why?')
 UserInput = input("You: ")
 
-if UserInput == 'Who are you?':
-  print('ChatBot1: I am ' + ChatBot_Name)
-  UserInput = input("You: ")
-if UserInput == 'Hi' :
-  print('ChatBot1: Hi')
-  UserInput = input('You: ') 
-if UserInput == "quit":
-  print('Ok ')
-  while True:
-    print('Goodbye')
-if UserInput == "what version is it?":
-  print("ChatBot: version 4.5 beta dev spot has closed.")
-  UserInput = input('You: ')
-if UserInput == 'I am sad':
- print('Chatbot: That is sad')
- UserInput = input('You: ')
-if UserInput == 'why':
-    print('ChatBot: idk')
+while (UserInput != "quit") :
+  if UserInput == 'Who are you?':
+    print('ChatBot1: I am ' + ChatBot_Name)
+    UserInput = input("You: ")
+  if UserInput == 'Hi' :
+    print('ChatBot1: Hi')
+    UserInput = input('You: ') 
+  if UserInput == "what version is it?":
+    print("ChatBot: version 4.5 beta dev spot has closed.")
     UserInput = input('You: ')
-if UserInput == 'But why?':
-  print('Chatbot: Ok i was just made because The bot dev was bored like this one')
+  if UserInput == 'I am sad':
+    print('Chatbot: That is sad')
   UserInput = input('You: ')
-if UserInput == 'Your not real':
-  print('ChatBot: Hey, I am a robot I am real what did I do? to get this')
-  UserInput = input('bad:')
-if UserInput == 'But really why?':
-  print('Chatbot: I get it Stop asking or else...')
-  UserInput = input('Crazy guy: ') 
+  if UserInput == 'why':
+      print('ChatBot: idk')
+      UserInput = input('You: ')
+  if UserInput == 'But why?':
+    print('Chatbot: Ok i was just made because The bot dev was bored like this one')
+    UserInput = input('You: ')
+  if UserInput == 'Your not real':
+    print('ChatBot: Hey, I am a robot I am real what did I do? to get this')
+    UserInput = input('bad:')
+  if UserInput == 'But really why?':
+    print('Chatbot: I get it Stop asking or else...')
+    UserInput = input('Crazy guy: ') 
 
+  print('i can not understand what you said ')
+  print('can you repeat that?')
+  UserInput = input("You: ")
 
   
